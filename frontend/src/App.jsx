d5f6c0210379b47b386db1c66f2056fba812b575
yakiTreeseason <maki05263@gmail.com>
2026-05-01 15:22:11 +0900

    feat:ReactからFASTAPIに投稿送信

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [message, setMessage] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   useEffect(() => {
     fetch("http://localhost:8000/hello") //API通信
       .then((res) => res.json()) //apiから帰ってきたデータをjson形式に変換
@@ -11,10 +13,39 @@ function App() {
         setMessage(data.message); //messageを置き換え取り出す
       });
   }, []); //最初の一回実行
+  //
+  const sendPost = async () => { //投稿処理
+    const res = await
+      fetch("http://localhost:8000/posts", {
+        method: "POST", //投稿作成
+        headers: {
+          "Content-Type": "application/json", //json形式で送ります
+        },
+        body: JSON.stringify({ //jsをJSON変換し、データを送る本体
+          title: title,
+          content: content,
+        }),
+      });
+    const data = await res.json(); //JSON変換
+    console.log(data);
+  }
   return (
     <div className="App" id='main'>
-      <h5>2ch風掲示板API-卒業制作</h5>
       <h1>{message}</h1>
+      <input type="text"
+        placeholder='タイトル'
+        value={title}
+        onChange={(e) =>
+          setTitle(e.target.value)}
+      />
+      <input type="text"
+        placeholder='内容'
+        value={content}
+        onChange={(e) =>
+          setContent(e.target.value)}
+      />
+      <br></br>
+      <button onClick={sendPost}>送信</button>
       <SignIn />
     </div>
   );
